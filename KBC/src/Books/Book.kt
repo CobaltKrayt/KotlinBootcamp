a97102d9e8814a35a0583650505c6cc6562c7340
CobaltKrayt <57502696+CobaltKrayt@users.noreply.github.com>
2020-10-11 16:27:08 +0300

    Collections, Constants :\

--- KBC/src/Books/Book.kt
+++ KBC/src/Books/Book.kt
@@ -1,8 +1,24 @@
 package Books
 
-class Book(var title: String, var author: String,var year: Int) {
+class Book(var title: String, var author: String,var year: Int,var booknumber: Int) {
 
     fun getTA() = title to author
 
     fun getTAY() = Triple(title,author,year)
+
+    //used this to implement top level constant MBN
+    fun canBorrow():Boolean{ if(booknumber>= MBN) return false else return true }
+
+    fun printUrl(){
+        println("${Constants.BASE_URL}${title}.html")
+    }
+
+    //using this to make BASE_URL accesible to book without using the Constants object
+    companion object{
+        val BASE_URL = "http://www.turtlecarecompobj.net/"
+    }
+}
+//Constant values under Constants object
+object Constants{
+    const val BASE_URL  = "http://www.turtlecare.net/"
 }
--- KBC/src/Books/bookmain.kt
+++ KBC/src/Books/bookmain.kt
@@ -1,9 +1,38 @@
 package Books
 
+const val MBN = 5
+
 fun main() {
 
+//Pair Triple -----------------------------------------------------------------------------------------
+    /*
     var greenbook = Book("Merry","Pippin",2020)
 
     println("Here is your book ${greenbook.title} wriiten by ${greenbook.author} in ${greenbook.year}")
+     */
+
+//Sets,Map,getOrPut ------------------------------------------------------------------------------------
+    /*
+    val allBooks = setOf("Macbeth", "A Midsummer Night's Dream","Hamlet")
+
+    val lib = mapOf("WS" to allBooks)
+
+    //when working with collections check if you need a value before handling what you want to a function
+    println(lib.any{it.value.contains("Hamlet")})
+
+    val moreBooks = mutableMapOf("Edgar Allan Poe" to "The Raven" )
+
+    moreBooks.getOrPut("Jules Verne"){"The Mysterious Island"}
+
+    moreBooks.getOrPut("Edgar Allan Poe"){"The Black Cat"}
+
+    println(moreBooks)
+
+     */
+
+    val b = Book
+
+
+
 
 }
